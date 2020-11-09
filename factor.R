bc94f67a920c402a6e9bbb61cc647da038b5cda7
Shitaye19 <shitmesk@yahoo.com>
2020-11-09 16:24:25 -0500

    Edit two codes on last excercise not working and lecture 15 writing function

--- factor.R
+++ factor.R
@@ -25,7 +25,8 @@ nlevels(h_countries)
 
 h_gap <- gapminder %>% 
   
-  filter(country%in% h_countries)
+  filter(country%in% h_countries) %>% 
+  droplevels()
 
 
 h_gap %>% 
@@ -123,13 +124,67 @@ fct_reorder(gapminder$country, gapminder$lifeExp,.desc = TRUE)%>%
   levels() %>% 
   head()
 
+gap_asia_2007_ordered <- gap_asia_2007 %>% 
+  mutate(country = fct_reorder(country, gdpPercap)) %>% 
+droplevels() # levles work with a single variable
+  
+levels(gap_asia_2007_ordered$country)
+
+h_gap
+
+p1 <-  ggplot(h_gap, aes(x = year, y = lifeExp, color = country))+
+geom_line()
+
+
+p2<- ggplot(h_gap, aes(x = year, y = lifeExp, color = fct_reorder2(country,year, lifeExp)))+
+  
+geom_line() +
+  labs(color = "country") # this is to color legnend
+
+grid.arrange(p1, p2, nrow = 1)
+
+
+##reorder2 to relavel based on two varibles
+
+h_gap$country %>%  levels()
+
+h_gap$country %>% fct_relevel("Egypt")
 
 
 
+
+
+####
+
+gap_asia_2007 <- gapminder %>% 
+  filter(year == 2007, continent == "Asia")
+
+gap_asia_2007$country %>% 
+  mutate(country = fct_relevel((fct_reorder(country, gdpPercap), Thiland))) # nested but the code is not working
+
+
+## Check from the course and edit even the function part 1 I didn't finish the code I have to get it and edit
+
+
+gap_asia_2007$country%>% 
+  mutate(country = fct_relevel(fct_reorder(country,gdpPercap)))%>% 
+           ggplot(aes(gdpPercap, fct_relevel(country, "Thailand")))+  ### not workign edit from the website
+           geom_point()
+         
   
-  
-  
-  
-  
+
+ 
+ 
+ i_gap <- gapminder %>% 
+   filter(country %in% c("United States", "Sweden", "Australia"))%>% 
+   droplevels()
+ 
+ i_gap$country %>%   
+   fct_recode("USA" ="United States", "Oz" = "Australia") %>% levels()
+      
+      
+      
+      
+      
   
   
